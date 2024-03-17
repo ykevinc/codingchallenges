456b41a40b5ad523332234ff962a6a7522558bc3
Kevin Chen <ykevinc@gmail.com>
2024-03-16 22:21:44 -0500

    chore: fmt

--- rust_l00001_two_sum/src/main.rs
+++ rust_l00001_two_sum/src/main.rs
@@ -1,29 +1,24 @@
-use std::collections::{HashMap};
-use std::collections::hash_map::Entry::{Occupied,Vacant};
+use std::collections::hash_map::Entry::{Occupied, Vacant};
+use std::collections::HashMap;
 
 fn main() {
     println!("Hello, world!");
 }
 
-
-struct Solution {
-}
-
+struct Solution {}
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-	let mut m: HashMap<i32,usize> = HashMap::new();
-	for (i,n) in nums.iter().enumerate() {
-		match m.entry(target-n) {
-			Occupied(e) => return vec![*(e.get()) as i32,i as i32],
-			Vacant(_e) => m.insert(*n, i),
-		};
-		
-	}
-	return vec![]
+        let mut m: HashMap<i32, usize> = HashMap::new();
+        for (i, n) in nums.iter().enumerate() {
+            match m.entry(target - n) {
+                Occupied(e) => return vec![*(e.get()) as i32, i as i32],
+                Vacant(_e) => m.insert(*n, i),
+            };
+        }
+        return vec![];
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -31,8 +26,7 @@ mod tests {
 
     #[test]
     fn create_new_rating() {
-	assert_eq!(Solution::two_sum(vec![], 3), vec![]);
-	assert_eq!(Solution::two_sum(vec![2,1], 3), vec![0,1])
-
+        assert_eq!(Solution::two_sum(vec![], 3), vec![]);
+        assert_eq!(Solution::two_sum(vec![2, 1], 3), vec![0, 1])
     }
 }
