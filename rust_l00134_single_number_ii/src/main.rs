d1d0af794b33c4c446e7dc4ef692e7c332beec79
Kevin Chen <ykevinc@gmail.com>
2025-01-02 20:53:24 -0600

    chore: fmt

--- rust_l00134_single_number_ii/src/main.rs
+++ rust_l00134_single_number_ii/src/main.rs
@@ -6,12 +6,12 @@ struct Solution {}
 
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> i32 {
-			let (mut c1, mut c2) = (0, 0);
-			for n in nums {
-				c1 = (c1^n) & !c2;
-				c2 = (c2^n) & !c1;
-			}
-			return c1
+        let (mut c1, mut c2) = (0, 0);
+        for n in nums {
+            c1 = (c1 ^ n) & !c2;
+            c2 = (c2 ^ n) & !c1;
+        }
+        return c1;
     }
 }
 
@@ -21,6 +21,6 @@ mod tests {
 
     #[test]
     fn test() {
-			assert_eq!(2, Solution::single_number(vec![1,1,1,2]));
+        assert_eq!(2, Solution::single_number(vec![1, 1, 1, 2]));
     }
 }
